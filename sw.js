0277a38be7cf619c7b9b7785140f3e4abf7b8f42
a5000original-cloud <a5000.original@gmail.com>
2026-09-18 21:06:10 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    URBaxA5000 · Service Worker · A5000 Labs
    ═══════════════════════════════════════════════════════════ */
 
-const CACHE_VERSION = 'v11';
+const CACHE_VERSION = 'v12';
 const CACHE_NAME    = `urbaxa-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `urbaxa-runtime-${CACHE_VERSION}`;
 const TILES_CACHE   = `urbaxa-tiles-${CACHE_VERSION}`;
